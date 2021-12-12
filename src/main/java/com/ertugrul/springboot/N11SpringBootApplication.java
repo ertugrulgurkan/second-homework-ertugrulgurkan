24d5c0393640715d7026702b880886fcbcfba8ac
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-12-12 19:10:47 +0300

    project created

---
+++ src/main/java/com/ertugrul/springboot/N11SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ertugrul.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N11SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N11SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ertugrul/springboot/N11SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ertugrul.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
